d56ed5d86e033fecacae03892ebee221f994a59b
Sam Gibson <sam.gibson@blist.com>
2010-02-12 23:03:41 -0600

    User login/logout service
    * Added a user sessions controller for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :badges
   map.resources :user_sessions
 
+  map.login '/login', :controller => 'user_sessions', :action => 'new'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
+
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
